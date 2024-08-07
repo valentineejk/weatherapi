f6a5e158b2d8396019d78617ddcaff359e7ada1d
valentine <valentineejk@icloud.com>
2024-08-07 10:44:10 +0100

    Initial Commit

---
+++ weatherapi/weatherapiApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherapiApp.swift
+//  weatherapi
+//
+//  Created by valentineejk on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct weatherapiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
